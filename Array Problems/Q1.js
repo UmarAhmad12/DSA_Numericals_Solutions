937f6d120b92df1950cb2e551d699cd7db2a3c64
UmarAhmad12 <umara6274@gmail.com>
2023-08-18 00:32:51 +0530

    Added Optimal solution

--- Array Problems/Q1.js
+++ Array Problems/Q1.js
@@ -13,10 +13,10 @@ Output: 10
 Explanation: 10 is the largest element in the array. 
  */
 
-                // Solution 1 --> Sorting
+                // Solution 1 --> Using Sorting
 
 /**
- * Brute force Approach: 
+ *     Brute force Approach: 
  * 
 Sort the array in ascending order.
 Print the (size of the array -1)th index.
@@ -43,3 +43,40 @@ const arr2 = [8,10,5,7,9];
 
 console.log(sortArr(arr1));     // OUT: 5
 console.log(sortArr(arr2));     // OUT: 10
+
+
+//                Solution 2 --> Using a max variable
+
+/**
+ *    Optimal Approach: 
+ * 
+Create a max variable and initialize it with arr[0].
+
+Use a for loop and compare it with other elements of the array
+
+If any element is greater than the max value, update max value with the element’s value
+Print the max variable.
+
+Time Complexity: O(N)
+
+Space Complexity: O(1)
+
+ */
+
+function findLargestElement(arr) {
+    let max = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+      if (max < arr[i]) {
+        max = arr[i];
+      }
+    }
+    return max;
+  }
+  
+  const arr3 = [2, 5, 1, 3, 0];
+  let result = findLargestElement(arr3);
+  console.log("The largest element in the array is: " + result);
+  
+  const arr4 = [8, 10, 5, 7, 9];
+  let result1 = findLargestElement(arr4);
+  console.log("The largest element in the array is: " + result1);
